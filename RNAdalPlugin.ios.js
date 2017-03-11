c22344755ccf9aa7b5b1f4506683415c0c473c23
Sam Colby <samcolby@users.noreply.github.com>
2017-03-11 19:17:47 +0000

    Fixed imports that now are set via main in package.json.

--- RNAdalPlugin.ios.js
+++ RNAdalPlugin.ios.js
@@ -2,22 +2,6 @@
  * @providesModule RNAdalPlugin
  * @flow
  */
-'use strict';
+import AuthenticationContext from './msadal/AuthenticationContext';
 
-import {
-  NativeModules
-} from 'react-native';
-
-var NativeRNAdalPlugin = NativeModules.RNAdalPlugin;
-
-/**
- * High-level docs for the RNAdalPlugin iOS API can be written here.
- */
-
-var RNAdalPlugin = {
-  test: function() {
-    NativeRNAdalPlugin.test();
-  }
-};
-
-module.exports = RNAdalPlugin;
+export default AuthenticationContext;
